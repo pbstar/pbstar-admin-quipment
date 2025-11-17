684c32ff60ba73ff8876129baf4dfa0650ecbb63
pengbo <pengbo18@shuidi-inc.com>
2025-11-17 20:13:15 +0800

    feat：子应用优化

--- src/main.js
+++ src/main.js
@@ -1,37 +1,5 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import ElementPlus from "element-plus";
-import zhCn from "element-plus/es/locale/lang/zh-cn";
-import router from "./router";
-import "element-plus/dist/index.css";
-import "element-plus/theme-chalk/dark/css-vars.css";
-import "@Passets/css/base.css";
+import { createMicroApp } from "../../app-common/appInit.js";
 import App from "./App.vue";
+import router from "./router";
 
-let instance = null;
-const render = () => {
-  const mainPinia = window.parent.$mainPinia;
-  instance = createApp(App);
-  instance.use(ElementPlus, {
-    locale: zhCn,
-  });
-  instance.use(router);
-  instance.use(createPinia());
-  instance.use(mainPinia);
-  instance.mount("#root");
-};
-if (window.__POWERED_BY_WUJIE__) {
-  window.__WUJIE_MOUNT = () => {
-    render(App);
-  };
-  window.__WUJIE_UNMOUNT = () => {
-    instance?.unmount();
-    instance = null;
-  };
-} else {
-  document.body.innerHTML = `
-    <div style="padding:10px;">
-      PbstarAdmin 子应用依赖父应用，不能独立运行！
-    </div>
-  `;
-}
+createMicroApp(App, [router]);
