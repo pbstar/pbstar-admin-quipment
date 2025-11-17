dcd1e7ee0aa3e3c87aad1c1b635867f3c8d1410e
pengbo <pengbo18@shuidi-inc.com>
2025-11-17 20:39:05 +0800

    feat：子应用优化

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,8 @@
 <script setup>
-import { RouterView, useRouter } from "vue-router";
-import { onUnmounted } from "vue";
-import useSharedStore from "@Passets/stores/shared";
-import { changeTheme } from "@Passets/utils/theme";
+import { RouterView } from "vue-router";
+import { useMicroApp } from "../../app-common/useMicroApp";
 
-const sharedStore = useSharedStore();
-const router = useRouter();
-// 子应用中添加处理
-const handleSharedPinia = (e) => {
-  for (const key in e) {
-    sharedStore[key] = e[key];
-    if (key == "isDark") {
-      changeTheme(e[key]);
-    }
-  }
-};
-window.$wujie?.props.path && router.push(window.$wujie.props.path);
-if (window.$wujie?.props.sharedPinia) {
-  handleSharedPinia(window.$wujie.props.sharedPinia);
-}
-window.$wujie?.bus.$on("changeSharedPinia", (e) => {
-  handleSharedPinia(e);
-});
-window.$wujie?.bus.$on("subappRouteChange", (obj) => {
-  if (obj && obj.path && obj.key == window.$wujie?.bus.id) {
-    router.push(obj.path);
-  }
-});
-onUnmounted(() => {
-  window.$wujie?.bus.$off("changeSharedPinia");
-  window.$wujie?.bus.$off("subappRouteChange");
-});
+useMicroApp();
 </script>
 
 <template>
